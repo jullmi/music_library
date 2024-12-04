7283c11320e3963c393e4820805cfca5cbd0eef4
Julia Savchenko <iusavchenko@dinord.ru>
2024-12-04 09:19:46 +0100

    added the starrt of migrations

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"music_library/config"
+	"music_library/db"
 )
 
-
-
-
-
-func main () {
+func main() {
 	config := config.LoadConfig()
 	log.Printf("Configuration loaded: %+v\n", config)
-	
+
+	databaseURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		config.DBUser,
+		config.DBPassword,
+		config.DBHost,
+		config.DBPort,
+		config.DBName,
+	)
+
+	migrationsPath := "db/migrations"
+	err := db.RunMigrations(databaseURL, migrationsPath)
+
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+
+	fmt.Println("Application started successfully!")
+
 }
